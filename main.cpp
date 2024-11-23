ee6b99d8be64c3bc00bcef3604c20304ec447955
Trisha Jain <trishajain12@Trishas-MacBook-Pro-2.local>
2024-11-22 17:37:43 -0800

    Main changes

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 #include <vector>
 #include <set>
-#include <cstdlib> // For rand()
-#include <ctime>   // For seeding rand()
+#include <cstdlib> 
+#include <ctime>  
 
 using namespace std;
 
@@ -13,17 +13,45 @@ double evaluate(const set<int>& features) {         // Dummy evaluation function
     return randomAccuracy;
 }
 
-int main(){
-    cout<< "Welcome to Trisha-Shreya's Feature Selection Algorithm."<<endl;
-    cout<<"Please enter total number of features: "<<endl;
+void forwardSelection(int numFeatures);
+
+void backwardSelection(int numFeatures); 
+
+void specialSelection(int numFeatures);
+
+int main() {
+    cout << "Welcome to Trisha-Shreya's Feature Selection Algorithm." << endl;
+
+    cout << "Please enter total number of features: " << endl;
     int numFeatures;
-    cin>> numFeatures;
-    cout<<"Type the number of the algorithm you want to run."<<endl;
-    cout<<"1. Forward Selection"<<endl;
-    cout<<"2. Backward Selection"<<endl;
-    cout<<"3. Our Special Algorithm"<<endl;
+    cin >> numFeatures;
+
+    cout << "Type the number of the algorithm you want to run." << endl;
+    cout << "1. Forward Selection" << endl;
+    cout << "2. Backward Selection" << endl;
+    cout << "3. Our Special Algorithm" << endl;
     int algType;
-    cin>> algType;
+    cin >> algType;
+
+    if (algType == 1)
+    {
+        forwardSelection(numFeatures);
+    }
+
+    else if (algType == 2)
+    {
+        backwardSelection(numFeatures); 
+    }
+
+    else if (algType == 3)
+    {
+        specialSelection(numFeatures);
+    }
+
+    else 
+    {
+        cout << "Invalid Entry" << endl; 
+    }
 
     return 0; 
 }
